6e28741675be4d15c8fa8b1baf5659522b68adf8
AlbaCastilla <castillamacki@gmail.com>
2024-11-13 19:22:32 +0100

    Conexion pagina formulario con firestore

---
+++ app/src/main/java/com/appclass/myapplication/screens/PantallaFormulario.kt
@@ -0,0 +1,10 @@
+package com.appclass.myapplication.screens
+
+import androidx.navigation.NavController
+import com.google.firebase.firestore.FirebaseFirestore
+
+fun FormularioDiario(navController: NavController) {
+    val db = FirebaseFirestore.getInstance()
+    val coleccion = "formularioDiario"
+
+}
