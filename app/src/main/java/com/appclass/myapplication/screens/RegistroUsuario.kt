00e8fb4c8a75de73ca02ffa715738d551cf35e11
beamm536 <beamm023@gmail.com>
2024-11-14 21:07:46 +0100

    teclado para el correo
    no lo aguantaba mas para hacer el testing, solo subo este cambio jajaj

--- app/src/main/java/com/appclass/myapplication/screens/RegistroUsuario.kt
+++ app/src/main/java/com/appclass/myapplication/screens/RegistroUsuario.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
@@ -40,6 +41,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -196,7 +198,8 @@ fun CamposRegistroUsuario(navController: NavController ,modifier: Modifier = Mod
                     unfocusedBorderColor = Color.Gray,
                     focusedBorderColor = MoradoTextFields,
                     cursorColor = MoradoTextFields
-                )
+                ),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email)
             )
         }
 
