9d7afd50130b18f1a21ed6ee5ec19d3480f2983a
Pedromrtz <pmartinezigual@gmail.com>
2024-11-19 13:18:54 +0100

    Calendario Terminado 2.0 y importaciones Navegación

--- app/src/main/java/com/appclass/myapplication/NavigationWrapper.kt
+++ app/src/main/java/com/appclass/myapplication/NavigationWrapper.kt
@@ -7,8 +7,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.appclass.myapplication.componentes.BottomNavigationBarComponent
 import com.appclass.myapplication.screens.ButtonAddForms
+import com.appclass.myapplication.screens.CalendarioApp
 import com.appclass.myapplication.screens.Formulario
+import com.appclass.myapplication.screens.InicioAppCRM
+import com.appclass.myapplication.screens.LoginUsuario
 /*
 import com.appclass.myapplication.componentes.BottomNavigationBarComponent
 import com.appclass.myapplication.screens.CalendarioApp
@@ -18,7 +22,9 @@ import com.appclass.myapplication.screens.PantallaInicio
 import com.appclass.myapplication.screens.RegistroUsuario
 import com.appclass.myapplication.screens.LoginUsuario*/
 import com.appclass.myapplication.screens.PantallaGraficos
+import com.appclass.myapplication.screens.PantallaInicio
 import com.appclass.myapplication.screens.Questionario
+import com.appclass.myapplication.screens.RegistroUsuario
 import com.google.firebase.firestore.FirebaseFirestore
 import java.time.LocalDate
 import java.util.UUID
@@ -31,8 +37,8 @@ import java.util.UUID
 @Composable
 fun NavigationWrapper(navHostController: NavHostController) {
 
-    NavHost(navController = navHostController, startDestination = "pantallaGraficos") {
-/*
+    NavHost(navController = navHostController, startDestination = "calendarApp") {
+
         composable("pantallaInicio") { PantallaInicio(navHostController) }
 
         composable("home") { BottomNavigationBarComponent(navHostController) }
@@ -45,7 +51,7 @@ fun NavigationWrapper(navHostController: NavHostController) {
 
         composable("calendarApp"){ CalendarioApp (navHostController) }
 
-*/
+
         composable("PantallaFormulario"){ Questionario (navHostController) }
         composable("pantallaGraficos") { PantallaGraficos(navHostController) }
         //para añadir los 29 días antes creo
