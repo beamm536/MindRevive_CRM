e46f2c01e49e2907a8f4faed32b3e82f15b2f68a
beamm536 <beamm023@gmail.com>
2024-11-25 20:15:09 +0100

    conseguido q se muestre el usuario registrado o logeado
    porfinnnnnn joderr lo q ha costado :)

--- app/src/main/java/com/appclass/myapplication/NavigationWrapper.kt
+++ app/src/main/java/com/appclass/myapplication/NavigationWrapper.kt
@@ -12,6 +12,7 @@ import com.appclass.myapplication.screens.Formulario
 
 import com.appclass.myapplication.componentes.BottomNavigationBarComponent
 import com.appclass.myapplication.screens.CalendarioApp
+//import com.appclass.myapplication.screens.EntradaApp
 import com.appclass.myapplication.screens.InicioAppCRM
 import com.appclass.myapplication.screens.PantallaInicio
 //import com.appclass.pruebasautentificacion.screens.LoginUsuario
